531d3696af216eb90dea153efc0ce79f7cdb879b
ohyo555 <555gywn@gmail.com>
2024-01-19 16:33:25 +0900

    1) 구구단 기초

--- src/main/java/com/KoreaIT/java/Jsp_AM/HomeMainServlet2.java
+++ src/main/java/com/KoreaIT/java/Jsp_AM/HomeMainServlet2.java
@@ -10,9 +10,29 @@ import java.io.IOException;
 @WebServlet("/home/main2")	
 public class HomeMainServlet2 extends HttpServlet {
 
+	private String i;
+	private CharSequence Gugudan;
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
-		response.getWriter().append("HelloWorld!!!!!!!!!!!!!!").append(request.getContextPath());
+		response.getWriter().append(gugudan(i)).append(request.getContextPath());
+	}
+
+	private CharSequence gugudan(String i) {
+		
+		String dan = i;
+		
+//		System.out.printf("==%d단==",i);
+		for(int a = 1; a <= 9; a++) {
+			
+			int answer = Integer.parseInt(dan) * a;
+			String num = Integer.toString(a);
+			String Gugudan = dan + " * " + num + " = " + answer;
+//			String[] g = 
+//			System.out.printf("%d * %d = %d",dan,a,answer);
+			
+		}
+		return Gugudan;
 	}
 
 
