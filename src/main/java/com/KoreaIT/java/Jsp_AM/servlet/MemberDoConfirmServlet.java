5ca3ccdf75f35ef1aa9d164683bc6f3ba773f01c
ohyo555 <555gywn@gmail.com>
2024-01-24 17:41:55 +0900

    *아이디 중복체크 하는중

--- src/main/java/com/KoreaIT/java/Jsp_AM/servlet/MemberDoConfirmServlet.java
+++ src/main/java/com/KoreaIT/java/Jsp_AM/servlet/MemberDoConfirmServlet.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Map;
 
 import com.KoreaIT.java.Jsp_AM.util.DBUtil;
 import com.KoreaIT.java.Jsp_AM.util.SecSql;
