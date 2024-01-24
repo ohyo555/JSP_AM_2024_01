0848a3eb162db83451b2c52729eb9f554a87b646
ohyo555 <555gywn@gmail.com>
2024-01-24 16:51:25 +0900

    회원가입 기능 구현

--- src/main/java/com/KoreaIT/java/Jsp_AM/servlet/MemberDoJoinServlet.java
+++ src/main/java/com/KoreaIT/java/Jsp_AM/servlet/MemberDoJoinServlet.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import com.KoreaIT.java.Jsp_AM.config.Config;
 import com.KoreaIT.java.Jsp_AM.util.DBUtil;
 import com.KoreaIT.java.Jsp_AM.util.SecSql;
 
@@ -22,36 +23,32 @@ public class MemberDoJoinServlet extends HttpServlet {
 		response.setContentType("text/html;charset=UTF-8");
 		// DB연결
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName(Config.getDbDriverClassName());
 		} catch (ClassNotFoundException e) {
 			System.out.println("클래스가 없습니다.");
 			e.printStackTrace();
 		}
 
-		String url = "jdbc:mysql://127.0.0.1:3306/JSP_AM?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
-		String user = "root";
-		String password = "";
-
 		Connection conn = null;
 
 		try {
-			conn = DriverManager.getConnection(url, user, password);
+			conn = DriverManager.getConnection(Config.getDbUrl(), Config.getDbUser(), Config.getDbPw());
 
-			String name = request.getParameter("name");
 			String loginId = request.getParameter("loginId");
 			String loginPw = request.getParameter("loginPw");
+			String name = request.getParameter("name");
 
 			SecSql sql = SecSql.from("INSERT INTO `member`");
 			sql.append("SET regDate = NOW(),");
 			sql.append("updateDate = NOW(),");
 			sql.append("loginId = ?,", loginId);
 			sql.append("loginPw = ?,", loginPw);
-			sql.append("`name` = ?;",name);
+			sql.append("`name` = ?;", name);
 
-			DBUtil.insert(conn, sql);
+			int id = DBUtil.insert(conn, sql);
 
-			response.getWriter()
-					.append(String.format("<script>alert('%s님 회원가입이 완료되었습니다.'); location.href=('../home/main');</script>", name));
+			response.getWriter().append(String.format(
+					"<script>alert('%s님, 회원가입이 완료되었습니다.'); location.replace('../article/list');</script>", name));
 
 		} catch (SQLException e) {
 			System.out.println("에러 : " + e);
