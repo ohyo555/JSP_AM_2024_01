4634797a53e0211b0e351640eccc557981da2611
ohyo555 <555gywn@gamil.com>
2024-01-26 13:23:07 +0900

    ArticleDao 도입

--- src/main/java/com/KoreaIT/java/Jsp_AM/dao/ArticleDao.java
+++ src/main/java/com/KoreaIT/java/Jsp_AM/dao/ArticleDao.java
@@ -1,38 +1,37 @@
 package com.KoreaIT.java.Jsp_AM.dao;
 
+import java.sql.Connection;
+import java.util.List;
+import java.util.Map;
+
+import com.KoreaIT.java.Jsp_AM.util.DBUtil;
 import com.KoreaIT.java.Jsp_AM.util.SecSql;
 
 public class ArticleDao {
+	private Connection conn;
 
-	public static SecSql countlist() {
+	public ArticleDao(Connection conn) {
+		this.conn = conn;
+	}
+
+	public int getArticlesTotalCount() {
 		SecSql sql = SecSql.from("SELECT COUNT(*) AS cnt");
 		sql.append("FROM article");
-		
-		return sql;
+
+		return DBUtil.selectRowIntValue(conn, sql);
 	}
 
-	public static SecSql list(int itemsInAPage, int limitFrom) {
-		
-		SecSql sql = SecSql.from("SELECT *");
-		sql.append("FROM article AS a");
-		sql.append("INNER JOIN `member` AS m");
-		sql.append("ON a.writer = m.id");
-		sql.append("ORDER BY a.id DESC");
+	public List<Map<String, Object>> getArticleRows(int limitFrom, int itemsInAPage) {
+		SecSql sql = SecSql.from("SELECT A.*, M.name AS writer");
+		sql.append("FROM article AS A");
+		sql.append("INNER JOIN `member` AS M");
+		sql.append("ON A.memberId = M.id");
+		sql.append("ORDER BY id DESC");
 		sql.append("LIMIT ?, ?;", limitFrom, itemsInAPage);
-		
-		return sql;
+
+		List<Map<String, Object>> articleRows = DBUtil.selectRows(conn, sql);
+
+		return articleRows;
 	}
 
-//	public SecSql dowrite(int loginedMemberId, String title, String body) {
-//		SecSql sql = SecSql.from("INSERT INTO article");
-//		sql.append("SET regDate = NOW(),");
-//		sql.append("writer = ?,", loginedMemberId);
-//		sql.append("title = ?,", title);
-//		sql.append("`body` = ?;", body);
-//		
-//		return sql;
-//	}
-
-	
-	
 }
